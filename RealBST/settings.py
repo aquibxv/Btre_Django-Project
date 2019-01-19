c2edbe623e46420eeaad0c4499bd866863fc6727
Aquib Ansari <aquibxv@gmail.com>
2019-01-19 13:22:38 +0530

    Commit 4

--- RealBST/settings.py
+++ RealBST/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
 ]
 
 MIDDLEWARE = [
