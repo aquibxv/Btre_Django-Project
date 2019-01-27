cf91f42fba2769d2fe54f4dcbb733a2461754f50
Aquib Ansari <aquibxv@gmail.com>
2019-01-27 10:40:12 +0530

    Update

--- RealBST/settings.py
+++ RealBST/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'RealBST.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'CodingTrain96',
+        'HOST': 'localhost', 
     }
 }
 
@@ -125,3 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'RealBST/static')
 ]
 
+
+# Media folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
